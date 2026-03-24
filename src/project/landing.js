b522b8d2fcf1626ce3e9439b0420614b0230e43a
BharathPenkey <124618939+BharathPenkey@users.noreply.github.com>
2026-03-24 19:07:30 +0530

    Frontend - Refactored Landing page

--- src/project/landing.js
+++ src/project/landing.js
@@ -2,20 +2,16 @@ import React from 'react';
 import "./landing.css";
 import landing_img from "./landing_img.png"
 import { Link } from "react-router-dom";
-function Landing(){
-    return(
+function Landing() {
+    return (
         <div className='landing'>
             <section className='landing_img'>
                 <img src={landing_img} alt='main'></img>
                 <section className='routing'>
-                <h1>10x team</h1>
-                <Link to={"/post"}><button>Enter</button></Link>
-                
+                    <h1>Hi! Welcome, great to have you here</h1>
+                    <Link to={"/post"}><button className='btn_ent'>Enter</button></Link>
+                </section>
             </section>
-
-            </section>
-            
-               
         </div>
     )
 }
