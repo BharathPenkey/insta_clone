6cc33f36a4ed44f7f57327b2497c0d0528cd7e23
BharathPenkey <124618939+BharathPenkey@users.noreply.github.com>
2026-03-24 18:59:04 +0530

    Frontend -form.jsx :Optimistic ui Updates

--- src/project/form.jsx
+++ src/project/form.jsx
@@ -1,60 +1,65 @@
-import {React,useState} from 'react';
-import { Link,useNavigate } from "react-router-dom";
+import { React, useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import insta_logo_img from "./insta_logo.png";
 import camera_icon from "./camera_icon.png";
 import './form.css';
 import axios from "axios";
-function Form(){
-    const current= new Date();
+function Form() {
+    const current = new Date();
     const navigate = useNavigate();
-    const up_date=`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
-    const[post,setPost]=useState({name:"",location:"",likes:"0",description:"",PostImage:"",date:""});
-    const [image,setImage]=useState('');
-    const handlesubmit= async (e)=>{
+    const up_date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`
+    const [post, setPost] = useState({ name: "", location: "", likes: "0", description: "", PostImage: "", date: "" });
+    const [image, setImage] = useState('');
+    const handlesubmit = async (e) => {
         e.preventDefault();
-        setPost({...post,date:up_date})
-        const data= new FormData();
-        data.append("file",image);
-        data.append("upload_preset","bharath");
-        data.append("cloud_name","drfg4tq7u");
-        await fetch("https://api.cloudinary.com/v1_1/drfg4tq7u/image/upload",{
+        setPost({ ...post, date: up_date })
+        const data = new FormData();
+        data.append("file", image);
+        data.append("upload_preset", "bharath");
+        data.append("cloud_name", "drfg4tq7u");
+        await fetch("https://api.cloudinary.com/v1_1/drfg4tq7u/image/upload", {
             //https://api.cloudinary.com/v1_1/drfg4tq7u   -->api base url
-            method:"post",
-            body:data
+            method: "post",
+            body: data
         })
-        .then(res=>res.json())
-        .then(data=>{
-            const newpost={
-                name:post.name,
-                location:post.location,
-                likes:post.likes,
-                description:post.description,
-                PostImage:data.url,
-                date:up_date
-            }
-             axios.post('https://insta-clone-ld6n.onrender.com/posts',newpost)//
-            
+            .then(res => res.json())
+            .then(data => {
+                const newpost = {
+                    name: post.name,
+                    location: post.location,
+                    likes: post.likes,
+                    description: post.description,
+                    PostImage: data.url,
+                    date: up_date
+                }
+                // axios.post('https://insta-clone-ld6n.onrender.com/posts', newpost)//
+                axios.post('https://insta-clone-b2.onrender.com/posts', newpost)//
+
             })
-        .catch(err=>console.log(err))
-            navigate("/post")
-      }
-    return(
+            .catch(err => console.log(err))
+        navigate("/post")
+    }
+    return (
         <>
-         <div className="main-header">
+            <div className="main-header">
                 <Link to={"/"}><img src={insta_logo_img} alt="logo" className="insta_logo"></img></Link>
                 <span className="tag">Instaclone</span>
+                  {/* <div id='addPost' onClick={()=>navigate("/form")}>ADD</div> */}
                 <Link to={"/form"}><img src={camera_icon} alt="icon" className="cam_logo"></img></Link>
-         </div>
-        <form>
-            <section>
-                <input type="file" id="myfile" name="myfile" onChange={(event)=>{setImage(event.target.files[0])}}/>
-                <input type="text"  id='author' placeholder='Author' onChange={(event)=>{setPost({...post,name:event.target.value})}}/>
-                <input type="text"  id="location" placeholder='Location' onChange={(event)=>{setPost({...post,location:event.target.value})}}/>
-                <input type="text" id="description" placeholder='Description' onChange={(event)=>{setPost({...post,description:event.target.value})}}/>
-                <button id="btn" onClick={handlesubmit}>Post</button>
-            </section>
-           
-        </form>
+            </div>
+            <form>
+                <section>
+                    <div className='containerBox1' onClick={()=>navigate("/post")}><span>&#10060;</span></div>
+                    <input type="file" id="myfile" name="myfile" onChange={(event) => { setImage(event.target.files[0]) }} />
+                    <input type="text" id='author' placeholder='Author' onChange={(event) => { setPost({ ...post, name: event.target.value }) }} />
+                    <input type="text" id="location" placeholder='Location' onChange={(event) => { setPost({ ...post, location: event.target.value }) }} />
+                    <input type="text" id="description" placeholder='Description' onChange={(event) => { setPost({ ...post, description: event.target.value }) }} />
+                    <div className='containerBox'>
+                        <button id="btn" onClick={handlesubmit}>Post</button>
+                    </div>
+                </section>
+
+            </form>
         </>
     )
 }
