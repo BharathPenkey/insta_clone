195847eaa53cb6eec8155d654762cd8f96d8a86e
BharathPenkey <124618939+BharathPenkey@users.noreply.github.com>
2026-03-24 15:01:30 +0530

    Update route.js

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router= express.Router();
-const Post = require("../Models/UserModel");
+const Post = require("../model");
 
 async function fixLikes() {
   try {
